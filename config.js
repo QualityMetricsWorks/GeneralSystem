3dccc8ec12d042808bde8bc3070d87f88db7245f
QualityMetricsWorks <quality.metricsworkssaltillo@gmail.com>
2026-08-27 15:14:12 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.QUALITY_SUMMARY_CONFIG = {
-  supabaseUrl: 'https://rdvqpxsmmgcbdjlolcni.supabase.co',
-  supabasePublishableKey: 'sb_publishable_qQjzWCHM-hwxm9uEHA1PqQ_L9NNSDu8'
+  supabaseUrl: 'https://fwmgxvzqxxzlrerokljs.supabase.co',
+  supabasePublishableKey: 'sb_publishable_7fTOQ38z2bxZqr3Wsgayrg_xuONQC5c'
 };
