39bd8815873e08a3563122f1a322a94fe756b31f
QualityMetricsWorks <quality.metricsworkssaltillo@gmail.com>
2026-08-30 23:49:14 -0600

    Update env.js

--- js/config/env.js
+++ js/config/env.js
@@ -1,1 +1,5 @@
-export const ENV={SUPABASE_URL:"https://ipcpnmpehqxstmrokycs.supabase.co",SUPABASE_ANON_KEY:"sb_publishable_VepM93n0q2MQtzrtNeP3iQ_HRldC1b9:"guvelsystems.com":"guvelsystems.com"};
+export const ENV = {
+  SUPABASE_URL: "https://ipcpnmpehqxstmrokycs.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwY3BubXBlaHF4c3Rtcm9reWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNDA4MjcsImV4cCI6MjEwMzcxNjgyN30.OEZgSX97N6sDmS-TTNMNU7O0qbG7lK39jbYKtW5j9dU",
+  ROOT_DOMAIN: "guvelsystems.com"
+};
